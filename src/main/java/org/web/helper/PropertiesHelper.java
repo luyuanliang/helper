02ce0bd577d55588a25a60423bb04e56f4075a55
luyl <tf56>
2018-08-04 11:30:12 +0800

    init

--- src/main/java/org/web/helper/PropertiesHelper.java
+++ src/main/java/org/web/helper/PropertiesHelper.java
@@ -9,69 +9,85 @@ import org.apache.log4j.Logger;
 
 public class PropertiesHelper {
 
-	private static Logger logger = Logger.getLogger(PropertiesHelper.class);
+    private static Logger logger = Logger.getLogger(PropertiesHelper.class);
 
-	public static String getValue(String key, String defaultValue) {
-		return getString(key, defaultValue);
-	}
+    public static String getString(ResourceBundle resourceBundle, String key, String defaultValue) {
+        try {
+            String value = resourceBundle.getString(key);
+            if (StringUtils.isEmpty(value)) {
+                return value;
+            } else {
+                logger.warn(key + "'s value is null ");
+            }
+        } catch (Exception e) {
+            logger.warn("warn key is " + key + "/n" + ServiceExceptionHelper.getExceptionInfo(e));
+        }
+        return defaultValue;
+    }
 
-	public static String getValue(String bundleName, String key, String defaultValue) {
-		String value = defaultValue;
-		try {
-			value = getString(bundleName, key);
-			if (StringUtils.isEmpty(value)) {
-				return value;
-			}
-		} catch (Exception e) {
-			logger.warn("warn key is " + key);
-			logger.warn(ServiceExceptionHelper.getExceptionInfo(e));
-		}
-		return value;
-	}
 
-	public static Boolean getValue(String bundleName, String key, boolean defaultValue) {
-		Boolean value = defaultValue;
-		try {
-			value = Boolean.valueOf(getString(bundleName, key));
-		} catch (Exception e) {
-			logger.warn("warn key is " + key);
-			logger.warn(ServiceExceptionHelper.getExceptionInfo(e));
-		}
-		return value;
-	}
+    public static Boolean getBoolean(ResourceBundle resourceBundle, String key, boolean defaultValue) {
+        try {
+            String value = resourceBundle.getString(key);
+            if (StringUtils.isEmpty(value)) {
+                return Boolean.valueOf(value);
+            } else {
+                logger.warn(key + "'s value is null ");
+            }
+        } catch (Exception e) {
+            logger.warn("warn key is " + key + "/n" + ServiceExceptionHelper.getExceptionInfo(e));
+        }
+        return defaultValue;
+    }
 
-	public static Long getValue(String bundleName, String key, Long defaultValue) {
-		Long value = defaultValue;
-		try {
-			value = Long.valueOf(getString(bundleName, key));
-		} catch (Exception e) {
-			logger.warn("warn key is " + key);
-			logger.warn(ServiceExceptionHelper.getExceptionInfo(e));
-		}
-		return value;
-	}
+    public static Long getLong(ResourceBundle resourceBundle, String key, Long defaultValue) {
+        try {
+            String value = resourceBundle.getString(key);
+            if (StringUtils.isEmpty(value)) {
+                return Long.valueOf(value);
+            } else {
+                logger.warn(key + "'s value is null ");
+            }
+        } catch (Exception e) {
+            logger.warn("warn key is " + key + "/n" + ServiceExceptionHelper.getExceptionInfo(e));
+        }
+        return defaultValue;
+    }
 
-	/**
-	 * 读取指定路径下的资源文件
-	 * 
-	 * @param bundleName
-	 *            properties文件路径
-	 * @param key
-	 * @return value
-	 */
-	public static String getString(String bundleName, String key) throws MissingResourceException {
-		return getResourceBundle(bundleName,null).getString(key);
-	}
+    public static Integer getInt(ResourceBundle resourceBundle, String key, Integer defaultValue) {
+        try {
+            String value = resourceBundle.getString(key);
+            if (StringUtils.isEmpty(value)) {
+                return Integer.valueOf(value);
+            } else {
+                logger.warn(key + "'s value is null ");
+            }
+        } catch (Exception e) {
+            logger.warn("warn key is " + key + "/n" + ServiceExceptionHelper.getExceptionInfo(e));
+        }
+        return defaultValue;
+    }
 
-	public static ResourceBundle getResourceBundle(String bundleName,String baseName) throws MissingResourceException {
-		Locale locale = null;
-		if(StringUtils.isEmpty(baseName)){
-			locale = Locale.getDefault();
-		} else {
-			locale = new Locale(baseName);
-		}
-		return ResourceBundle.getBundle(bundleName, locale);
-	}
+    /**
+     * 读取指定路径下的资源文件
+     *
+     * @param bundleName properties文件路径
+     * @param key
+     * @return value
+     */
+    public static String getString(String bundleName, String key) throws MissingResourceException {
+        return getResourceBundle(bundleName, null).getString(key);
+    }
+
+    public static ResourceBundle getResourceBundle(String bundleName, String baseName) throws MissingResourceException {
+        Locale locale = null;
+        if (StringUtils.isEmpty(baseName)) {
+            locale = Locale.getDefault();
+        } else {
+            locale = new Locale(baseName);
+        }
+        return ResourceBundle.getBundle(bundleName, locale);
+    }
 
 
 }
